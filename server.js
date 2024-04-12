8c5fd798d99db996d4ad8d9d44cfbeac3f9d1ed5
Christopher Brown <bchristopher240@yahoo.com>
2024-04-12 01:26:55 -0400

    troubleshoot alter server.js

--- server.js
+++ server.js
@@ -1,14 +1,52 @@
-// server.js
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-const typeDefs = require('./graphql/schema.js');
-const resolvers = require('./graphql/resolvers');
-const connectDB = require('./config/db');
-const { verifyToken } = require('./helpers/auth');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const dotenv = require('dotenv');
 
-const app = express();
-connectDB();
+// Importing schema
+const typeDefs = require('./graphql/schema');
 
+dotenv.config();
+
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Define User model
+const User = mongoose.model('User', new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+}));
+
+// GraphQL resolvers
+const resolvers = {
+  Query: {
+    currentUser: (_, __, { user }) => {
+      return user;
+    },
+  },
+  Mutation: {
+    login: async (_, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user || !(await bcrypt.compare(password, user.password))) {
+        throw new Error('Invalid email or password');
+      }
+      return jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+    },
+    signup: async (_, { username, email, password }) => {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = await User.create({ username, email, password: hashedPassword });
+      return jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+    },
+  },
+};
+
+// Apollo Server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -19,10 +57,19 @@ const server = new ApolloServer({
   },
 });
 
-server.applyMiddleware({ app });
+// Express App
+const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Start Apollo Server
+async function startServer() {
+  await server.start();
+  server.applyMiddleware({ app });
 
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
-);
+  // Start server
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+startServer();
