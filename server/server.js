091cd694086ac934067ae6fa040986a648ab21e9
Christopher Brown <bchristopher240@yahoo.com>
2024-04-12 01:45:46 -0400

    added resolvers loc

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const { ApolloServer, gql } = require('apollo-server-express');
 const typeDefs = require('./graphql/schema');
-const resolvers = require('./resolvers/userResolvers');
+const resolvers = require('./graphql/resolvers');
 
 const server = new ApolloServer({
   typeDefs,
